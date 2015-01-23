f940489e38ef357df37799471dabffb60104f588
Bjarne Riis <riis.bjarne@icloud.com>
2015-01-23 14:07:41 +0100

    Added base project

---
+++ NLog.Targets.Sentry/SentryTarget.cs
@@ -0,0 +1,26 @@
+﻿using NLog.Config;
+using SharpRaven;
+
+namespace NLog.Targets.Sentry
+{
+    [Target("Sentry")]
+    public class SentryTarget : Target
+    {
+        [RequiredParameter]
+        public string Dsn { get; set; }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            var sentryClient = new RavenClient(Dsn);
+
+            if (logEvent.Exception != null)
+            {
+                sentryClient.CaptureException(logEvent.Exception, logEvent.Message);
+            }
+            else
+            {
+                sentryClient.CaptureMessage(logEvent.Message);
+            }
+        }
+    }
+}
