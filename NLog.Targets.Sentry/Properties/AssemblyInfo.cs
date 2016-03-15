8369015b0f7843085f66cb70499a6fffd4412a8e
Bjarne Riis <riis.bjarne@icloud.com>
2016-03-15 09:57:44 +0100

    Version 1.3 Added the option to pass LogEventInfo as tags

--- NLog.Targets.Sentry/Properties/AssemblyInfo.cs
+++ NLog.Targets.Sentry/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bjarne Riis, Dillon Buchanan")]
 [assembly: AssemblyProduct("NLog.Targets.Sentry")]
-[assembly: AssemblyCopyright("Copyright © 2015 Bjarne Riis")]
+[assembly: AssemblyCopyright("Copyright © 2016 Bjarne Riis")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
 [assembly: InternalsVisibleTo("NLog.Targets.Sentry.UnitTests")]
